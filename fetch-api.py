522d91d7dcb9574c6e9f9b620ba78d9b1858bdd4
zidraf123 <zidraf123@gmail.com>
2026-05-15 13:51:19 +0700

    Create a function to return user IPv6 address

--- fetch-api.py
+++ fetch-api.py
@@ -2,3 +2,10 @@ from dotenv import load_dotenv
 import requests
 import os
 import socket
+
+def get_ip6():
+    hostname = socket.gethostname()
+    addresses = socket.getaddrinfo(hostname, None, socket.AF_INET6)
+    user_ip = addresses[2][4][0]
+
+    return user_ip
