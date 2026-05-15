159b325f0b71114db7a9547f4b5ad2be94931db9
zidraf123 <zidraf123@gmail.com>
2026-05-15 14:53:49 +0700

    Create a function to fetch location API from ip6
    - Move the api_key variable into the fetch_ip_geo_api because the api_key
      is only used inside there
    - Add the API endpoint (url) to connect to API
    - Add simple status code handling which will return the data in json format
      if the status code is 200 (success) and return None if it's not success

--- fetch-api.py
+++ fetch-api.py
@@ -11,4 +11,19 @@ def get_ip6():
 
     return user_ip
 
-iplocate_api_key = os.getenv("IPLOCATE_API_KEY")
+def fetch_ip_geo_api():
+    load_dotenv()
+    iplocate_api_key = os.getenv("IPLOCATE_API_KEY")
+    ip6 = get_ip6()
+
+    BASE_URL = "https://iplocate.io/api/lookup/"
+    url = f"{BASE_URL}{ip6}?apikey={iplocate_api_key}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        print("success!")
+        return data
+    else:
+        print(f"ERROR: {response.status_code}")
+        return None
