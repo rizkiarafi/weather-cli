ac5eb41cef86fbdfae8680f47ca49f2e15953763
zidraf123 <zidraf123@gmail.com>
2026-05-17 00:47:18 +0700

    Update the way of handling API requests errors
    In this commit I assume that the only problem is whether users have connection
    or not. There are 2 nested conditionals, in this project users need to know
    whether users had been online before or not, and within it we need to know
    if we have internet connection or not. These nested conditionals are needed for
    getting user IPv6 and calling geolocation API. This is how it's handled:
    
    - The way of getting IPv6 is through socket and there is no API needed,
      but if users have no internet connection, it also means no IPv6. If
      since the very first launch users is offline, then the IPv6 will be set to `None`,
      but if at least users had launched it online already, then the IPv6 will
      be its cache
    - As long as users have the IPv6, online, and assuming there are no other
      problems, users can always call for IP to geolocation API response and
      then write geolocation cache, but if the geolocation cache have already
      existed and the IPv6 is the same as the previous IPv6 used, then use the
      geolocation cache. And if the opposite case occurs, use the cache if exists,
      and return `None` if not.
    
    The decision to return `None` if API call / its cache load failed is meant for
    showing more clear messages to users that they have to connect to internet first.

--- fetch-api.py
+++ fetch-api.py
@@ -4,26 +4,62 @@ import os
 import socket
 import json
 
+def load_json(data_file):
+    try:
+        with open(data_file, "r") as read:
+            ip_cache = json.load(read)
+            return ip_cache
+    except FileNotFoundError:
+        return None
+
+def write_json(content, data_file):
+    with open(data_file, "w") as write:
+        json.dump(content, write, indent=2)
+
 def get_ip6():
     hostname = socket.gethostname()
     addresses = socket.getaddrinfo(hostname, None, socket.AF_INET6)
-    user_ip = addresses[2][4][0]
+    try:
+        user_ip = {"ip": addresses[1][4][0]}
+        ip6 = user_ip["ip"]
+        write_json(user_ip, "ip-cache.json")
+    except IndexError:
+        ip_cache = load_json("ip-cache.json")
+        ip6 = ip_cache["ip"] if ip_cache != None else None
 
-    return user_ip
+    return ip6
 
 def fetch_ip_geo_api():
     load_dotenv()
     iplocate_api_key = os.getenv("IPLOCATE_API_KEY")
+
     ip6 = get_ip6()
+
+    geo_data = None
+    geo_cache = load_json("ip-geo-cache.json")
 
     BASE_URL = "https://iplocate.io/api/lookup/"
     url = f"{BASE_URL}{ip6}?apikey={iplocate_api_key}"
 
+    if geo_cache:
+        if ip6 == geo_cache["user_ip"]:
+            print("Using cache if exists when the IP is the same with previous IP")
+            geo_data = geo_cache
+            return geo_data
+    
     response = requests.get(url)
+
     if response.status_code == 200:
-        data = response.json()
-        print("success!")
-        return data
+        geo_data = response.json()
+        geo_data["user_ip"] = ip6
+        write_json(geo_data, "ip-geo-cache.json")
+        print("Requesting success!")
     else:
-        print(f"ERROR: {response.status_code}")
-        return None
+        if geo_cache:
+            geo_data = geo_cache
+            print("Using cache if exists when the API throws an error")
+            print(f"ERROR: {response.status_code}")
+
+    return geo_data
+
+print(fetch_ip_geo_api())
